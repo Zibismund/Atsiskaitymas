a0dd45170d51c1715de23b62fad87059cacc59ff
Zibismund <zbignev1982@gmail.com>
2024-02-18 18:41:32 +0200

    first commit--dependencies+properties(mysql connection)+app starter

---
+++ src/main/java/lt/codeacademy/javau8/archyvas/ArchyvasPagrindApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.javau8.archyvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchyvasPagrindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchyvasPagrindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/codeacademy/javau8/archyvas/ArchyvasPagrindApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.javau8.archyvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchyvasPagrindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
